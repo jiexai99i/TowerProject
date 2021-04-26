8ef669f170f0070f3cb31f9e6578a143351f3c45
jiexai99i <jiexai99u@yandex.ru>
2021-04-26 13:25:43 +0500

    tower

---
+++ TowerProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int target = 0;
+    public Transform exit;
+    public Transform[] points;
+    public float navigation;
+
+    private Transform enemy;
+    private float navigationTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (points != null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigation)
+            {
+                if (target < points.Length)
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, points[target].position, navigationTime);
+                }
+
+                else
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exit.position, navigationTime);
+                }
+                navigationTime = 0;
+            }
+        }
+                    
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Point")
+        {
+            target += 1;
+        }
+        else if (collision.tag == "Finish")
+        {
+            Manager.Instance.RemoveEnemyFromScrenn();
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ TowerProject/Assets/Scripts/Manager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : Loader<Manager>
+{   public GameObject spawnPoints;
+    public GameObject[] enemy;
+    public int maxEnemesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerSpawn;
+   
+    private int enemiesOnScreen = 0;
+    private const float spawnDelay = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+
+    IEnumerator Spawn()
+    {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies)
+        {
+            for (int i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (enemiesOnScreen < maxEnemesOnScreen)
+                {
+                    GameObject newEnemy = Instantiate(enemy[1]) as GameObject;
+                    newEnemy.transform.position = spawnPoints.transform.position;
+                    enemiesOnScreen += 1;
+                }
+            }
+            
+            yield return new WaitForSeconds(spawnDelay);
+            StartCoroutine(Spawn());
+        }
+        
+
+    }
+
+    public void RemoveEnemyFromScrenn()
+    {
+        if (enemiesOnScreen > 0)
+        {
+            enemiesOnScreen -= 0;
+        }
+    }
+
+}
